db38117e8d490ba0641d1e445c10b9be043b8f1f
benjaminlopezyambot <benjaminlopezyambot@gmail.com>
2023-02-28 20:30:43 +0700

    Initial

--- app/page.js
+++ app/page.js
@@ -1,91 +1,29 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import data from '../data.json';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+function LinkCard({href, title, image}) {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <a href={href}  className="flex items-center w-full p-1 rounded-md 
+    hover:scale-105 transition-all  mb-3 text-gray-800 bg-gray-100">
+      <div className="flex  w-full text-center">
+    <Image className="rounded-sm" alt={title}  src={image} width={30} height={30} />
+
+        <h2 className="font-semibold w-full text-center">{title}</h2>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </a>
   )
 }
+
+export default function Home() {
+return(
+  <div className="flex mx-auto flex-col items-center px-8 w-full justify-center mt-16 ">
+    <Image className="rounded-full" alt={data.name}  src={data.avatar} width={96} height={96} />
+    <h1 className="font-semibold mt-4 mb-8 text-lg">{data.name}</h1>
+    {data.links.map((item)=> (
+      <LinkCard key={item.href} {...item}/>
+    ))}
+  </div>
+)
+}
+
+
--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {domains: ['z-p3-scontent.fpnh5-2.fna.fbcdn.net','1000logos.net','cdn-icons-png.flaticon.com']},
   experimental: {
     appDir: true,
   },
 }
 
 module.exports = nextConfig
+
